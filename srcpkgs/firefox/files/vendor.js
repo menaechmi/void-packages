dca7a0fc4dccf3c662a1bba47f972326616a95c4
menaechmi <menaechmi@protonmail.com>
2024-10-23 15:15:33 -0500

    firefox: remove mozilla location services

--- srcpkgs/firefox/files/vendor.js
+++ srcpkgs/firefox/files/vendor.js
@@ -7,5 +7,3 @@ pref("browser.shell.checkDefaultBrowser", false);
 // Don't disable our bundled extensions in the application directory
 pref("extensions.autoDisableScopes", 11);
 
-// The default is google and we don't have api keys for it.
-pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
